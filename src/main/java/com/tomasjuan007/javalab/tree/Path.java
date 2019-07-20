7e92e6b2eff27d4cc7864491cd8e59fb42588f9d
Tomas <18819451598@163.com>
2019-07-20 16:38:52 +0800

    Refactor path resolver test

--- src/main/java/com/tomasjuan007/javalab/tree/Path.java
+++ src/main/java/com/tomasjuan007/javalab/tree/Path.java
@@ -15,22 +15,6 @@ public class Path {
         steps.add(step);
     }
 
-    public boolean equals(Object obj) {
-        boolean result = true;
-        if (!(obj instanceof Path)) {
-            result = false;
-        } else {
-            Path other = (Path) obj;
-            for (int index = 0; index < steps.size(); index++) {
-                if (other.steps.size() < index || !steps.get(index).equals(other.steps.get(index))) {
-                    result = false;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-
     public String toString() {
         return "[Path]"+steps.toString();
     }
--- src/main/java/com/tomasjuan007/javalab/tree/ResolverTest.java
+++ src/main/java/com/tomasjuan007/javalab/tree/ResolverTest.java
@@ -5,46 +5,47 @@ import java.util.List;
 
 public class ResolverTest {
 
-    private static StepDefinition _100_101 = new StepDefinition("1.0.0", "1.0.1");
-    private static StepDefinition _101_102 = new StepDefinition("1.0.1", "1.0.2");
-    private static StepDefinition _102_103 = new StepDefinition("1.0.2", "1.0.3");
-    private static StepDefinition _101_103 = new StepDefinition("1.0.1", "1.0.3");
-    private static StepDefinition _103_105 = new StepDefinition("1.0.3", "1.0.5");
-    private static StepDefinition _105_110 = new StepDefinition("1.0.5", "1.1.0");
+    private static StepDefinition _A_B = new StepDefinition("A", "B");
+    private static StepDefinition _B_C = new StepDefinition("B", "C");
+    private static StepDefinition _C_D = new StepDefinition("C", "D");
+    private static StepDefinition _B_D = new StepDefinition("B", "D");
+    private static StepDefinition _D_E = new StepDefinition("D", "E");
+    private static StepDefinition _C_E = new StepDefinition("A", "D");
     private static List<StepDefinition> steps = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
 
-        steps.add(_100_101);
-        steps.add(_101_102);
-        steps.add(_102_103);
-        steps.add(_101_103);
-        steps.add(_103_105);
-        steps.add(_105_110);
+        steps.add(_A_B);
+        steps.add(_B_C);
+        steps.add(_C_D);
+        steps.add(_B_D);
+        steps.add(_D_E);
+        steps.add(_C_E);
         PathResolver pathResolver = new PathResolver(steps);
 
-        Path bestPath = pathResolver.findBestPath("1.0.0", "1.0.3");
-        Path resultPath = new Path();
-        resultPath.addStep(_100_101);
-        resultPath.addStep(_101_103);
-//        boolean flag = true;
-//        for (int index = 0; index < bestPath.getSteps().size(); index++) {
-//            if (resultPath.getSteps().size() < index || !steps.get(index).equals(resultPath.getSteps().get(index))) {
-//                flag = false;
-//            }
-//        }
-//        if (!flag) throw new Exception();
-        if (!bestPath.equals(resultPath)) {
-            throw new Exception();
-        }
-        System.out.println();
-        bestPath = pathResolver.findBestPath("1.0.1", "1.0.5");
-        Path resultPath2 = new Path();
-        resultPath2.addStep(_101_103);
-        resultPath2.addStep(_103_105);
-        if (!bestPath.equals(resultPath2)) {
-            throw new Exception();
-        }
+        Path bestPath = pathResolver.findBestPath("A", "D");
+        System.out.println(verify(bestPath, _A_B, _B_D));
+        
+        bestPath = pathResolver.findBestPath("B", "E");
+        System.out.println(verify(bestPath, _B_D, _D_E));
 
+        bestPath = pathResolver.findBestPath("A", "E");
+        System.out.println(bestPath);
+    }
+
+    public static boolean verify(Path bestPath, StepDefinition... args) throws Exception {
+        boolean result = true;
+        int argsSize = args.length;
+        List<StepDefinition> bestPathSteps = bestPath.getSteps();
+        if (bestPathSteps.size() != argsSize) {
+            return false;
+        }
+        for (int index = 0; index < bestPathSteps.size(); index++) {
+            if (!bestPathSteps.get(index).equals(args[index])) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
